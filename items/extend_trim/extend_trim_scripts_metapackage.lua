46921269cc922ec4484cb8b651a839fce42e18c8
jkooks <juliuskukl@gmail.com>
2026-01-23 08:23:04 -0800

    fix @provides tag take three

--- items/extend_trim/extend_trim_scripts_metapackage.lua
+++ items/extend_trim/extend_trim_scripts_metapackage.lua
@@ -6,8 +6,10 @@
 -- @link https://github.com/jkooks/jk_reascripts
 -- @metapackage
 -- @provides
---	 [main] jk_extend_trim_options.lua
---	 [main] jk_extend_trim_left_edge_no_fade.lua
---	 [main] jk_extend_trim_right_edge_no_fade.lua
---	 [main] jk_extend_trim_tail_within_bounds.lua
---	 [main] jk_extend_trim_top_within_bounds.lua
+--		[main] jk_extend_trim_options.lua
+--		[main] jk_extend_trim_left_edge_no_fade.lua
+--		[main] jk_extend_trim_right_edge_no_fade.lua
+--		[main] jk_extend_trim_tail_within_bounds.lua
+--		[main] jk_extend_trim_top_within_bounds.lua
+
+do return end
