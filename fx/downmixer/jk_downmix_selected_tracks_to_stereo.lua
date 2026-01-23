ed5223944fd2243fd2196ca926a4b038305a2caf
jkooks <97080611+jkooks@users.noreply.github.com>
2026-01-22 23:22:21 -0800

    update downmixer script indexing

--- fx/downmixer/jk_downmix_selected_tracks_to_stereo.lua
+++ fx/downmixer/jk_downmix_selected_tracks_to_stereo.lua
@@ -1,10 +1,9 @@
 -- @description Downmix Selected Tracks To Stereo
--- @about Adds the Downmixer plugin to the selected tracks and downmixes them to stereo
--- Distributed under the GNU GPL v3 License. See license.txt for more information.
+-- @about
+--		Adds the Downmixer plugin to the selected tracks and downmixes them to stereo
+-- 		Distributed under the GNU GPL v3 License. See license.txt for more information.
 -- @author Julius Kukla
 -- @version 0.0.1
--- @provides jk_api/jk_api.lua
--- @provides jk_api/modules/jk_fx_api.lua
 
 -- load APIs
 package.path = reaper.GetResourcePath() .. "/Scripts/jk_reascripts/jk_api/?.lua"
