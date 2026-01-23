381a6f5dd9f9b6cf052d46a102a1ecc8d28590d7
jkooks <juliuskukl@gmail.com>
2026-01-23 08:29:02 -0800

    fix @provides value

--- fx/downmixer/downmixer_scripts_metapackage.lua
+++ fx/downmixer/downmixer_scripts_metapackage.lua
@@ -1,14 +1,8 @@
 -- @description Downmixer Scripts
 -- @about Scripts that add the Downmixer plugin to either selected tracks or takes.
---	 Distributed under the GNU GPL v3 License. See license.txt for more information.
+--      Distributed under the GNU GPL v3 License. See license.txt for more information.
 -- @author jkooks
 -- @version 0.0.1
 -- @link https://github.com/jkooks/jk_reascripts
 -- @metapackage
--- @provides
--- 		[main] jk_downmix_selected_takes_to_mono.lua
--- 		[main] jk_downmix_selected_takes_to_stereo.lua
---		[main] jk_downmix_selected_tracks_to_mono.lua
---		[main] jk_downmix_selected_tracks_to_stereo.lua
-
-do return end
+-- @provides [main] jk_downmix_selected_takes_to_mono.lua [main] jk_downmix_selected_takes_to_stereo.lua [main] jk_downmix_selected_tracks_to_mono.lua [main] jk_downmix_selected_tracks_to_stereo.lua
