6a17a6d54d6a8a56d7189d62b9adc4cf99ca8e8b
jkooks <97080611+jkooks@users.noreply.github.com>
2026-01-22 23:35:46 -0800

    fix api indexing (again)

--- jk_api/jk_api.lua
+++ jk_api/jk_api.lua
@@ -1,23 +1,23 @@
 -- @description Utility API Modules
 -- @about Extended API module for everyday use
 -- 		Distributed under the GNU GPL v3 License. See license.txt for more information.
+--
+--		## Using the API
+-- 		To import this extended API into a file, copy the below code:
+--
+-- 		'''
+-- 		package.path = reaper.GetResourcePath() .. "/Scripts/jk_reascripts/jk_api/?.lua"
+-- 		local jk = require "jk_api"
+-- 		'''
+--
+-- 		For any additional modules, please load them using the helper functions (see Loading section below).
+--
 -- @author Julius Kukla
 -- @version 0.0.0
 -- @provides
 -- 		[nomain] .
--- 		[nomain] jk_api/modules/*.lua
 -- 		jk_api/modules/*.lua
 
---[[
-	To import this extended API into a file, copy the below code:
-	
-	'''
-		package.path = reaper.GetResourcePath() .. "/Scripts/jk_reascripts/jk_api/?.lua"
-		local jk = require "jk_api"
-	'''
-	
-	For any additional modules, please load them using the helper functions (see Loading section below).
-]]--
 
 local jk = {}
 
